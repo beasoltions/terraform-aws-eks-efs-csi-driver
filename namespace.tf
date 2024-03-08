777bce239d752e564a43cb9d76421b109f49df93
Deen Muhammad <40787426+deenMuhammad@users.noreply.github.com>
2024-03-08 23:42:01 +0900

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,6 +1,7 @@
+data "kubernetes_all_namespaces" "allns" {}
+
 resource "kubernetes_namespace" "kubernetes_efs_csi_driver" {
-  depends_on = [var.mod_dependency]
-  count      = (var.enabled && var.create_namespace && var.namespace != "kube-system") ? 1 : 0
+  count = (contains(data.kubernetes_all_namespaces.allns.namespaces, var.namespace)) ? 0 : 1
 
   metadata {
     name = var.namespace
